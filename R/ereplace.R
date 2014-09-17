ea93d4b43f291f6c39bb6ad34940438a586bdabc
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-17 19:47:31 -0400

    readme

--- R/ereplace.R
+++ R/ereplace.R
@@ -1,7 +1,7 @@
 # DT %>% ereplace(as.character,"v1*")
 
 
-ereplace=function(DT,fun,cols=names(DT),i=TRUE,by=NULL,...){
+ereplace=function(DT,fun,cols=names(DT),filter=TRUE,by=NULL,...){
   func=as.character(substitute(fun))
   colsc=unlist(str_split(cols,pattern=" "))
   cc=NULL
@@ -9,6 +9,6 @@ ereplace=function(DT,fun,cols=names(DT),i=TRUE,by=NULL,...){
     cc=c(cc,names(DT)[which(str_detect(names(DT),glob2rx(c)))])
   }
   colsc= cc
-  eval(substitute(DT[i,colsc:=lapply(.SD,function(x){fun(x,...)}),by,.SD=colsc]))
+  eval(substitute(DT[filter,colsc:=lapply(.SD,function(x){fun(x,...)}),by,.SD=colsc]))
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,20 @@ stataR
 ======
 
 A tentative package for Stata users
+
+````R
+DT %>% set(order,"v\*")
+DT %>% set(sort,"v\*")
+DT %>% set(sort,"v?")
+DT %>% set(rename,"v1","v11")
+
+
+DT %>% select(id) %>% describe
+DT %>% select(id) %>% filter(v1==2) %>% describe(,d=T)
+
+
+
+DT %>% egen(mean,"v1","v1_mean",by=year,filter=id==3)
+DT %>% ereplace(mean,"v*",by=year,filter=id==3)
+DT %>% ereplace(as.character,"\*",by=year,filter=id==3)
+````
