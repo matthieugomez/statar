a150b4b1765ea93921c2cbc5e35fe79ebf63ad47
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 16:50:55 -0400

    emptyrlhd

--- R/spread.R
+++ R/spread.R
@@ -1,8 +1,16 @@
 #' @export
 spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FALSE) {
-  response=setdiff(names(data),c(key_col,value_col))
+  response <- setdiff(names(data), c(key_col, value_col))
+  length_lhs <- length(response)
+  if (!length_lhs) {
+  	response <- tempname("temp",DT)
+  	data[,(tempvar):=.N] 
+  }
   formula <- reformulate(termlabels = key_col , response = response)
   data2 <- dcast.data.table(data, formula, fill = fill, value.var = value_col)
+  if (!length_lhs) {
+  	data[,(tempvar):=NULL]
+  }
   if (convert) {
      data2[] <- lapply(data2, type.convert, as.is = TRUE)
    }
