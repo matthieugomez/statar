e8e9ce22fa14fe6269e9c489944086b69de00fa6
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 16:09:18 -0400

    option convert

--- R/gather.R
+++ R/gather.R
@@ -1,9 +1,12 @@
 #' @export
 gather_.data.table <- function(data, key_col, value_col, gather_cols,
-                               na.rm = FALSE) {
-  print("ok")
+                               na.rm = FALSE, convert = FALSE) {
   data2 <- data.table::melt(data, measure.vars = gather_cols,
     variable.name = key_col, value.name = value_col, na.rm = na.rm)
+  if (convert) {
+    data2[[key_col]] <- type.convert(as.character(data2[[key_col]]),
+      as.is = TRUE)
+  }
   data2
 }
 
