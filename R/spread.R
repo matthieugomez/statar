54963d7f02e65b3a224727437aa7f0e20b52a5a9
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 16:37:23 -0400

    rewrite

--- R/spread.R
+++ R/spread.R
@@ -1,7 +1,8 @@
 #' @export
 spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FALSE) {
-  formula <- reformulate(termlabels = value_col , response = key_col)
-  data2 <- dcast.data.table(data, formula)
+  response=setdiff(names(DT),c(key_col,value_col))
+  formula <- reformulate(termlabels = key_col , response = response)
+  data2 <- dcast.data.table(data, formula, fill = fill, value.var = value.col)
   if (convert) {
      data2[] <- lapply(data2, type.convert, as.is = TRUE)
    }
