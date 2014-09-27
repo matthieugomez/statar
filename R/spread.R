255260ed763c74456a7d42819ed753b836188f6c
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 16:23:56 -0400

    spread

--- R/spread.R
+++ R/spread.R
@@ -1,6 +1,9 @@
 #' @export
-spread_.data.table <- function(data, key_col, value_col) {
+spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FALSE) {
   formula <- reformulate(termlabels = value_col , response = key_col)
+  if (convert) {
+     ordered[] <- lapply(ordered, type.convert, as.is = TRUE)
+   }
   data2 <- data.table.dcast(DT, formula)
   data2
 }
--- R/statar.R
+++ R/statar.R
@@ -2,6 +2,5 @@
 #' @docType package
 #' @name statar
 #' @import data.table
-#' @importFrom tidyr gather_
 #' @importFrom tidyr spread_
 NULL
