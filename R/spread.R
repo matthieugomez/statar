0c70fcc3ccc712ee07c4ffc273c80c02636f9f4e
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 16:28:53 -0400

    roxygen

--- R/spread.R
+++ R/spread.R
@@ -1,10 +1,10 @@
 #' @export
 spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FALSE) {
   formula <- reformulate(termlabels = value_col , response = key_col)
+  data2 <- dcast.data.table(data, formula)
   if (convert) {
-     ordered[] <- lapply(ordered, type.convert, as.is = TRUE)
+     data2[] <- lapply(data2, type.convert, as.is = TRUE)
    }
-  data2 <- dcast.data.table(DT, formula)
   data2
 }
 
