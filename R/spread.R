739c2f185ecec0430f457dc50ca5eb8e46a2ee21
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 16:54:08 -0400

    response

--- R/spread.R
+++ R/spread.R
@@ -4,12 +4,12 @@ spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FA
   length_lhs <- length(response)
   if (!length_lhs) {
   	response <- tempname("temp",data)
-  	data[,(tempvar):=.N] 
+  	data[,(response):=.N] 
   }
   formula <- reformulate(termlabels = key_col , response = response)
   data2 <- dcast.data.table(data, formula, fill = fill, value.var = value_col)
   if (!length_lhs) {
-  	data[,(tempvar):=NULL]
+  	data[,(response):=NULL]
   }
   if (convert) {
      data2[] <- lapply(data2, type.convert, as.is = TRUE)
