d76641de20d8bcffb73ec1418e00fd3b70c3f5a2
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 17:27:23 -0400

    gather

--- R/spread.R
+++ R/spread.R
@@ -4,15 +4,19 @@ spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FA
   length_lhs <- length(response)
   if (!length_lhs) {
   	response <- tempname("temp",data)
-  	data[,(response):=.N] 
+  	data[, (response):=1:.N] 
   }
+  else{
+  	if (anyDuplicated(data, by = c(response))) stop("Duplicate identifiers for rows")
+  	}
   formula <- reformulate(termlabels = key_col , response = response)
-  data2 <- dcast.data.table(data, formula, fill = fill, value.var = value_col)
+  data2 <- dcast.data.table(data, formula, value.var = value_col, fill = fill)
   if (!length_lhs) {
-  	data[,(response):=NULL]
+  	data[, (response) := NULL]
+  	data2[, (response) := NULL]
   }
   if (convert) {
-     data2[] <- lapply(data2, type.convert, as.is = TRUE)
+     data2[, names(data2) := lapply(.SD,type.convert, as.is = TRUE),.SDcols=names(data2)]
    }
   data2
 }
