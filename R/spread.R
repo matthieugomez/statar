dfe03cc0f158bfeebb38723093fc9814c093df82
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-27 17:30:17 -0400

    spread

--- R/spread.R
+++ R/spread.R
@@ -3,8 +3,8 @@ spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FA
   response <- setdiff(names(data), c(key_col, value_col))
   length_lhs <- length(response)
   if (!length_lhs) {
-  	response <- tempname("temp",data)
-  	data[, (response):=1:.N] 
+  	response <- tempname("temp", data)
+  	data[, (response) := 1:.N] 
   }
   else{
   	if (anyDuplicated(data, by = c(response))) stop("Duplicate identifiers for rows")
@@ -16,7 +16,7 @@ spread_.data.table <- function(data, key_col, value_col, fill = NA, convert = FA
   	data2[, (response) := NULL]
   }
   if (convert) {
-     data2[, names(data2) := lapply(.SD,type.convert, as.is = TRUE),.SDcols=names(data2)]
+     data2[, names(data2) := lapply(.SD,type.convert, as.is = TRUE), .SDcols=names(data2)]
    }
   data2
 }
