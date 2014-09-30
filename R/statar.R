cf07ce611ee11861a768ec012767f219399ed42e
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-09-30 11:30:09 -0400

    roxygen

--- R/statar.R
+++ R/statar.R
@@ -1,4 +1,5 @@
-#' The statar package.
+#' The statar package
+#'
 #' @docType package
 #' @name statar
 #' @import data.table 
@@ -8,8 +9,6 @@
 #' @importFrom dplyr select_
 #' @importFrom dplyr tbl_vars
 #' @importFrom dplyr groups
-#' @importFrom dplyr order_with
-#' @importFrom dplyr order_with
 #' @importFrom lazyeval lazy_dots
 #' @importFrom lazyeval common_env
 #' @importFrom lubridate update
@@ -17,3 +16,5 @@
 #' @importFrom parallel mcMap
 #' @importFrom stringr str_replace
 #' @importFrom tidyr spread_
+NULL
+
