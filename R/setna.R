bfe968bc9810c6f0cfdaca4662fbeba05c0de5bf
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-10-03 11:37:41 -0400

    setna

--- R/setna.R
+++ R/setna.R
@@ -11,28 +11,17 @@
 #'  value = c(4.1, NA, MA, 5.3, 3.0, 3.2, 5.2)
 #' )
 #' setkey(DT, id, date)
-#' fill_na(DT, value, roll = TRUE)
-#' @name fill_na
+#' setna(DT, value, roll = TRUE)
+#' @name setna
 NULL
 
 
 
-fill_na <- function(.data, ...,roll = TRUE ,  rollends = if (roll=="nearest") c(TRUE,TRUE)
+
+setna <- function(.data, ...,.dots, roll = TRUE ,  rollends = if (roll=="nearest") c(TRUE,TRUE)
   else if (roll>=0) c(FALSE,TRUE)
   else c(TRUE,FALSE)){
-	fill_na_(.data, .dots = lazy_dots(...), roll = TRUE, rollends = rollends)
-}
-
-
-fill_na_ <- function(.data, ...,.dots, roll = TRUE ,  rollends = if (roll=="nearest") c(TRUE,TRUE)
-  else if (roll>=0) c(FALSE,TRUE)
-  else c(TRUE,FALSE)){
-  	keys <- key(.data)    
-	dots <- all_dots(.dots, ...)
-	vars <- names(select_vars_(names(.data), dots, exclude = keys))
-	if (length(vars) == 0) {
-	vars <- lazy_dots(everything())
-	}
+  keys <- key(.data)    
   if (length(keys)<1){
     stop(".data must be keyed by at least one variable")
   }   
@@ -41,7 +30,6 @@ fill_na_ <- function(.data, ...,.dots, roll = TRUE ,  rollends = if (roll=="near
   }
 }
 
-vars <- select_vars_(tbl_vars(.data), vars, exclude = as.character(groups(.data)))
 
 
 
