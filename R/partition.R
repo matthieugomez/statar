f53de67387e59643faccc4942d1237e67242e350
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-10-09 19:20:25 -0400

    docs

--- R/partition.R
+++ R/partition.R
@@ -2,7 +2,7 @@
 #'
 #' @param x A vector
 #' @param cutpoints Cutpoints to use when \code{nq} is not specified.  For instance \code{cutpoints = 0.4} creates two groups, one for observations equal or below 0.4, one for observations superior to 0.4. Cutpoints must be unique.
-#' @param prob A vector of probabilities. Can be used instead of cutpoints. Quantiles are computed as the inverse of the empirical distribution function (type = 1)
+#' @param prob A vector of probabilities that an be used instead of cutpoints. Quantiles are computed as the inverse of the empirical distribution function (type = 1)
 #' @param n_quantiles A numeric specifying number of quantiles. Can be used instead of cutpoints
 #' @return An integer vector representing groups separated by cutpoints. Includes missing values when present in the original vector.
 #' @examples 
@@ -11,10 +11,10 @@
 #' partition(v, prob = c(0.3, 0.7)) # 3 groups based on terciles
 #' partition(v, cutpoints = c(2, 3)) # 3 groups based on two cutpoints
 #' @export
-partition <- function(x, cutpoints = NULL, probabilities = NULL, n_quantiles = NULL){
+partition <- function(x, cutpoints = NULL, prob = NULL, n_quantiles = NULL){
   if (!is.null(n_quantiles)){
   if (!is.null(cutpoints)|!is.null(prob)) stop("Only one option among cutpoints, prob and n_quantiles can be used")
-      breaks <- tail(head(quantile(x, seq(0, 1, length = n + 1), type = 1, na.rm = TRUE), -1),-1)
+      breaks <- tail(head(quantile(x, seq(0, 1, length = n_quantiles + 1), type = 1, na.rm = TRUE), -1),-1)
       return(partition(x, cutpoints = breaks))
   } else if (!is.null(prob)){
 	  	if (!is.null(cutpoints)) stop("Only one option among cutpoints, prob and n_quantiles can be used")
