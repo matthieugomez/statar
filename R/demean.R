18d82192de969c7f181ae28e990523aba0dd9050
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-10-18 19:12:22 -0400

    demean

--- R/demean.R
+++ R/demean.R
@@ -5,9 +5,9 @@
 #' @return A demeaned vector
 #' @details This function calls felm::demeanlist after dealing with missing values and converting group variables into factors
 #' @examples       
-#' demean(c(1,2), c(1,1))  
-#' demean(c(NA,2), list(c(1,2), c(1,3)))               
-#' demean(c(1,2), list(c(NA,2), c(1,3)))
+#' demean(c(1,2), fe = c(1,1))  
+#' demean(c(NA,2), fe = list(c(1,2), c(1,3)))               
+#' demean(c(1,2), fe = list(c(NA,2), c(1,3)))
 #' @export
 demean <- function(x, fe){
 	stopifnot(is.atomic(x))
