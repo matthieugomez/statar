92ee109377d60ec77c18f371c408cc47a9c5c0f3
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-10-18 18:34:00 -0400

    demean vector

--- R/demean.R
+++ R/demean.R
@@ -1,42 +1,32 @@
-#' Demean a set of variables in a data.table
+#' Demean a vector 
 #' 
-#' @param x A data.table
-#' @param ... variables to demean
-#' @param fe List of groups to use
-#' @return data.table with demeaned variables (NA if one of the variable is missing), with suffix "_demean"
+#' @param x Vector to deman
+#' @param fe List of groups
+#' @return A demeaned vector
 #' @details This function calls felm::demeanlist after checking for missing values and converting group variables into factors
-#' @examples                        
-#' N=1e3; K=100
-#' set.seed(1)
-#' DT <- data.table(
-#'   id = sample(5, N, TRUE),
-#'   v1 =  sample(5, N, TRUE),                          
-#'   v2 =  sample(1e6, N, TRUE),                        
-#'   v3 =  sample(round(runif(100,max=100),4), N, TRUE) 
-#' )
-#' demean(DT, list(v2,v3), fe = list(id,v1))
+#' @examples       
+#' demean(c(1,2), c(1,1))  
+#' demean(c(NA,2), list(c(1,2), c(1,3)))               
+#' demean(c(1,2), list(c(NA,2), c(1,3)))
 
 #' @export
-demean <- function(x, ..., fe, suffix = "_demean", inplace = FALSE){
-	  demean_(x, .dots = lazy_dots(...), fe = substitute(fe), suffix = suffix)
+demean <- function(x, fe){
+	stopifnot(is.atomic(x))
+	if (is.double(x)){
+		x <- as.double(x)
 	}
-
-demean_ <- function(x, ..., .dots, fe, suffix = "_demean", inplace = FALSE){
-	stopifnot(is.data.table(x))
-	dots <- all_dots(.dots, ...)
-	vars <- names(select_vars_(names(x), dots))
-	fe <- names(select_vars_(names(x), fe))
-	ans <- x[, c(vars, fe), with = FALSE]
-	rows <- rowSums(as.matrix(is.na(ans)))==0
-	for (v in vars){
-		if (!ans[,is.double(get(v))]) ans[, (v) := as.double(get(v))]
+	if (is.atomic(fe)){
+		fe <- list(fe)
 	}
-	for (v in fe){
+	ans <- c(list(x), fe)
+	setDT(ans)
+	rows <- rowSums(is.na(ans))==0
+	ans <- ans[rows]
+	for (v in names(ans)[2:length(ans)]){
 		if (!ans[,is.factor(get(v))]) ans[, (v) := as.factor(get(v))]
 	}
-	m <- demeanlist(ans[, vars, with = FALSE], ans[, fe, with = FALSE])
-	if (!inplace){
-		x <- copy(x)
-	}
-	x[rows, paste(vars,suffix,sep ="_") := as.list(m)]
+	m <- unlist(demeanlist(ans[, "V1", with = FALSE], ans[, names(ans)[-1], with = FALSE]))
+	out <- rep(NA, length(x))
+	out[rows] <- m
+	out
 }
