5d4c21d47ed2162e3ad658757d5a10398483e099
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.princeton.edu>
2014-10-18 19:02:03 -0400

    demean vector

--- R/demean.R
+++ R/demean.R
@@ -1,14 +1,13 @@
 #' Demean a vector 
 #' 
 #' @param x Vector to deman
-#' @param fe List of groups
+#' @param fe List of vectors for group (factor, characters or integers)
 #' @return A demeaned vector
-#' @details This function calls felm::demeanlist after checking for missing values and converting group variables into factors
+#' @details This function calls felm::demeanlist after dealing with missing values and converting group variables into factors
 #' @examples       
 #' demean(c(1,2), c(1,1))  
 #' demean(c(NA,2), list(c(1,2), c(1,3)))               
 #' demean(c(1,2), list(c(NA,2), c(1,3)))
-
 #' @export
 demean <- function(x, fe){
 	stopifnot(is.atomic(x))
@@ -20,7 +19,7 @@ demean <- function(x, fe){
 	}
 	ans <- c(list(x), fe)
 	setDT(ans)
-	rows <- rowSums(is.na(ans))==0
+	rows <- !data.table:::is_na(ans)
 	ans <- ans[rows]
 	for (v in names(ans)[2:length(ans)]){
 		if (!ans[,is.factor(get(v))]) ans[, (v) := as.factor(get(v))]
