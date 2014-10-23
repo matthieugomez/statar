c899cb2cea0aec11f073c4567f91c23e2c9208a6
matthieugomez <Matthieu@nat-oitwireless-inside-vapornet100-c-30896.Princeton.EDU>
2014-10-22 21:32:00 -0400

    try

--- R/setselect.R
+++ R/setselect.R
@@ -1,8 +1,10 @@
-#' Retain certain columns of a data.table in place (= Stata keep).
+#' Drop columns or rows in place 
 #'
 #' @param x a data.table 
-#' @param ... Variables to keep. See the \link[dplyr]{select} documentation.
+#' @param ... Variables to keep. Default to all. See the \link[dplyr]{select} documentation.
 #' @param .dots Used to work around non-standard evaluation.
+#' @param i Condition within groups
+#' @param by Groups by which condition should be evaluated
 #' @examples
 #' library(data.table)
 #' DT <- data.table(
@@ -13,25 +15,21 @@
 #' setkeep(DT, i = id==1)
 #' setkeep(DT, id, v2)
 #' setkeep(DT, -id)
+#' setkeep(DT, i = id==1)
+#' setkeep(DT, id, v2)
+#' setkeep(DT, -id)
+
 #' @export
 setkeep <- function(x, ...,i = NULL, by = NULL){
-	x <- substitute(x)
 	setkeep_(x = x, .dots = lazyeval::lazy_dots(...), i = substitute(i), by = substitute(by))
 }
 
 #' @export
 #' @rdname setkeep
 setkeep_ <- function(x, ..., .dots, i = NULL, by = NULL){
-	if (is.name(x)){
-		env <- parent.frame(2)
-		true_x <- as.character(x)
-		x <- eval(x, parent.frame())
-	} else{
-		env <- parent.frame()
-		true_x <- as.character(substitute(x))
-	}
+	env_x <- as.lazy(x)$env
+	name_x <- as.character(as.lazy(x)$expression)
 	stopifnot(is.data.table(x))
-	xsub <- substitute(x)
 	byvars <- names(select_vars_(names(x), by))
 	if (!length(by)){
 	    byvars <- NULL
@@ -41,14 +39,14 @@ setkeep_ <- function(x, ..., .dots, i = NULL, by = NULL){
 	if (!length(vars))  vars <- names(x)
 	if (!is.null(i)){
 		if (is.null(by)){
-			assign(true_x, filter_(x, i), env)
+			assign(name_x, filter_(x, i), env)
 		} else{
 			expr <- lapply(dots, `[[`, "expr")
 			call <- substitute(dt[, .I[expr], by = vars], list(expr = dplyr:::and_expr(expr)))
 			env <- dt_env(x, lazyeval::common_env(dots), by = byvars)
 			ans <- eval(call, env)
 			indices <- ans[[length(ans)]]
-			assign(true_x, x[indices[!is.na(indices)]], env)
+			assign(name_x, x[indices[!is.na(indices)]], env)
 		}
 	}
 	drop <- setdiff(copy(names(x)), vars)
@@ -58,6 +56,61 @@ setkeep_ <- function(x, ..., .dots, i = NULL, by = NULL){
 	}
 }
 
+
+#' Create new data.table with columns or rows
+#'
+#' @param x a data.table 
+#' @param ... Variables to keep. Default to all. See the \link[dplyr]{select} documentation.
+#' @param .dots Used to work around non-standard evaluation.
+#' @param i Condition within groups
+#' @param by Groups by which condition should be evaluated
+#' @examples
+#' library(data.table)
+#' DT <- data.table(
+#'   id = c(1,2),
+#'   v1 = c(1,1),
+#'   v2 = c(2,1)
+#' )
+#' setkeep(DT, i = id==1)
+#' setkeep(DT, id, v2)
+#' setkeep(DT, -id)
+#' setkeep(DT, i = id==1)
+#' setkeep(DT, id, v2)
+#' setkeep(DT, -id)
+
+#' @export
+keep <- function(x, ...,i = NULL, by = NULL){
+	keep_(x = x, .dots = lazyeval::lazy_dots(...), i = substitute(i), by = substitute(by))
+}
+
+#' @export
+#' @rdname keep
+keep_ <- function(x, ...,i = NULL, by = NULL){
+		stopifnot(is.data.table(x))
+		byvars <- names(select_vars_(names(x), by))
+		if (!length(by)){
+		    byvars <- NULL
+		}
+		dots <- lazyeval::all_dots(.dots, ...)
+		vars <- names(select_vars_(names(x), dots))
+		if (!length(vars))  vars <- names(x)
+		if (!is.null(i)){
+			if (is.null(by)){
+				x <-  filter_(x, i)
+			} else{
+				expr <- lapply(dots, `[[`, "expr")
+				call <- substitute(dt[, .I[expr], by = vars], list(expr = dplyr:::and_expr(expr)))
+				env <- dt_env(x, lazyeval::common_env(dots), by = byvars)
+				ans <- eval(call, env)
+				indices <- ans[[length(ans)]]
+				x <-  x[indices[!is.na(indices)]]
+			}
+		}
+		drop <- setdiff(copy(names(x)), vars)
+		if (length(drop)>0){
+			x <- select(x, .dots= vars)
+		}
+	}
 
 
 
