690eee51a2898b0a3d4bb0758bd6f9f41e89f83c
matthieugomez <Matthieu@nat-oitwireless-inside-vapornet100-c-30896.Princeton.EDU>
2014-10-25 18:04:41 -0400

    updated documentation

--- R/date.R
+++ R/date.R
@@ -1,18 +1,24 @@
-#' Elapsed calendar periods
+#' Elapsed dates (weekly, monthly, quarterly)
 #'
-#' @param date Any vector that has a method for "
 #' @examples 
 #' date <- mdy(c("04/03/1992", "01/04/1992", "03/15/1992"))  
-#' l <- data.frame(
-#'    weekly = as.weekly(date), 
-#'    monthly = as.monthly(date), 
-#'    quarterly = as.quarterly(date)
-#'    )
-#' l
-#' l+1
-#' l %>% mutate_each(funs(week))
-#' l %>% mutate_each(funs(lag(c(1,2,3), along_with = .)))
-#' @details Weekly, monthly and quarterly dates are dates stored as the number of elapsed calendar periods since January 01 1970. This is helpful for two reasons. First, it allows to use methods defined on numeric on quarterly date (for instance lag wrt one period). Moreover, these classes are printed in a way that fits their frequency YYYYqQ, YYYYmMM, YYYYmWW.  Methods to convert from and to Dates or POSIXlt are provided.
+#' datem <-  as.monthly(date)
+#' # Print depends on period
+#' datem
+#' as.character(datem)
+#' # Internally stocked as inter, which allows things like:
+#' datem+1
+#' lag(c(1, 2, 3), n = 1, along_with = datem)
+#' # Behaves as a date for lubridate operation
+#' week(datem)
+#' # Convert to usual date format if needed
+#' as.POSIXlt(datem)
+#' as.POSIXct(datem)
+#' as.Date(datem)
+#' @details Weekly, monthly and quarterly dates are stored as the number of elapsed calendar periods since January 01 1970.
+#'  This allows to use methods defined on numeric on quarterly date (for instance lag wrt one period). Moreover, these classes are printed in a way that fits their frequency YYYYqQ, YYYYmMM, YYYYmWW.  
+#'
+#' Methods to convert from and to Dates or POSIXlt are provided. In particular, you may use lubridate \code{\link{week}} \code{\link{month}} and \code{\link{year}} to extract informations from elapsed dates.
 #' @name elapsed
 #' @aliases quarterly, monthly, weekly
 NULL
@@ -42,45 +48,17 @@ as.monthly <- function(date){
 #' @export
 #' @rdname elapsed 
 as.weekly <- function(date){
-    date <- floor_date(as.POSIXlt(date), "week")
+    date <- as.POSIXlt(date)
     date_origin <- as.POSIXlt(mdy("01/01/1970"))
-    out <- 53L*(date$year-date_origin$year) + ((date$yday)%/%7 -date_origin$mon)
+    out <- 53L*(date$year-date_origin$year) + ((date$yday)%/%7 - date_origin$yday)
     attributes(out) <- NULL
     class(out) <-  "weekly"
     out
 }
 
-# as.POSIXlt
-#' @export
-#' @rdname elapsed 
-as.POSIXlt.quarterly <- function(date, ...){
-  attributes(date) <- NULL
-  date_origin <- mdy("01/01/1970")
-  date <- date_origin+ 3L*months(date)
-  as.POSIXlt(date, ...)
-}
-
-#' @export
-#' @rdname elapsed 
-as.POSIXlt.monthly <- function(date, ...){
-  attributes(date) <- NULL
-  date_origin <- mdy("01/01/1970")
-  date <- date_origin+ months(date)
-  as.POSIXlt(date, ...)
-}
-
-#' @export
-#' @rdname elapsed 
-as.POSIXlt.weekly<- function(date, ...){
-  attributes(date) <- NULL
-  date_origin <- mdy("01/01/1970")
-  date <- date_origin+ weeks(date)
-  as.POSIXlt(date, ...)
-}
 
 # as.Date
 #' @export
-#' @rdname elapsed 
 as.Date.quarterly <- function(date, ...){
   attributes(date) <- NULL
   date_origin <- mdy("01/01/1970")
@@ -89,7 +67,6 @@ as.Date.quarterly <- function(date, ...){
 }
 
 #' @export
-#' @rdname elapsed 
 as.Date.monthly <- function(date, ...){
   attributes(date) <- NULL
   date_origin <- mdy("01/01/1970")
@@ -98,76 +75,119 @@ as.Date.monthly <- function(date, ...){
 }
 
 #' @export
-#' @rdname elapsed 
 as.Date.weekly<- function(date, ...){
   attributes(date) <- NULL
   date_origin <- mdy("01/01/1970")
-  date <- date_origin+ weeks(date)
+  date <- date_origin + years(floor(date %/% 53)) + weeks(date-53*floor(date %/% 53))
   as.Date(date, ...)
 }
 
 
-
-# format (align on m and q)
+# as.character
 #' @export
-#' @rdname elapsed 
-format.quarterly <- function(date, ...){
-  format(paste0(year(date),"q", quarter(date)), ...)
+as.character.quarterly <- function(date, ...){
+  paste0(year(date),"q", quarter(date))
 }
 
 #' @export
-#' @rdname elapsed 
-format.monthly <- function(date, ...){
+as.character.monthly <- function(date, ...){
   month <- month(date)
   monthc <- as.character(month)
   monthcc <- paste0("0",monthc[which(month<10)])
   monthc[which(month<10)] <- monthcc
-  format(paste0(year(date),"m", monthc), ...)
+  paste0(year(date),"m", monthc)
 }
 
 #' @export
-#' @rdname elapsed 
-format.weekly <- function(date, ...){
+as.character.weekly <- function(date, ...){
   week <- week(date)
   weekc <- as.character(week)
   weekcc <- paste0("0", weekc[which(week<10)])
   weekc[which(week<10)] <- weekcc
-  format(paste0(year(date),"w", weekc), ...)
+  paste0(year(date),"w", weekc)
 }
 
-# stupid stuff
-#' @export
-#' @rdname elapsed 
-as.data.frame.monthly <- function(...){
-      as.data.frame.vector(...)
-}
+
+
+# Stupid
 
 #' @export
-#' @rdname elapsed 
-as.data.frame.quarterly <- function(...){
-    as.data.frame.vector(...)
+as.POSIXlt.quarterly <- function(date, ...){
+  as.POSIXlt(as.Date(date), ...)
 }
 
 #' @export
-#' @rdname elapsed 
-as.data.frame.weekly <- function(...){
-    as.data.frame.vector(...)
+as.POSIXlt.monthly <- function(date, ...){
+  as.POSIXlt(as.Date(date), ...)
 }
 
 #' @export
-#' @rdname elapsed 
+as.POSIXlt.weekly<- function(date, ...){
+  as.POSIXlt(as.Date(date), ...)
+}
+
+
+#' @export
+as.POSIXct.quarterly <- function(date, ...){
+  as.POSIXct(as.Date(date), ...)
+}
+
+#' @export
+as.POSIXct.monthly <- function(date, ...){
+  as.POSIXct(as.Date(date), ...)
+}
+
+#' @export
+as.POSIXct.weekly<- function(date, ...){
+  as.POSIXct(as.Date(date), ...)
+}
+
+
+
+#' @export
+format.quarterly <- function(date, ...){
+  format(as.character(date))
+}
+
+#' @export
+format.monthly <- function(date, ...){
+  format(as.character(date))
+}
+
+#' @export
+format.weekly <- function(date, ...){
+  format(as.character(date))
+}
+
+#' @export
 print.quarterly <- function(date){
   print(format(date))
 }
 
 #' @export
-#' @rdname elapsed 
 print.monthly <- function(date){
   print(format(date))
 }
 
 #' @export
-#' @rdname elapsed 
 print.weekly <- function(date){
   print(format(date))
 }
+
+# stupid stuff
+#' @export
+as.data.frame.monthly <- function(...){
+    as.data.frame.vector(...)
+}
+
+#' @export
+as.data.frame.quarterly <- function(...){
+    as.data.frame.vector(...)
+}
+
+#' @export
+as.data.frame.weekly <- function(...){
+    as.data.frame.vector(...)
+}
+
+
