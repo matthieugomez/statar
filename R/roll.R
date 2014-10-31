4332eba197bb1ca95b39bbf3c083ed4a69e81aa6
matthieugomez <Matthieu@nat-oitwireless-inside-vapornet100-c-30896.Princeton.EDU>
2014-10-31 16:09:36 -0400

    add closed argument

--- R/roll.R
+++ R/roll.R
@@ -5,6 +5,7 @@
 #' @param n an integer specifying the rolling window
 #' @param order_by override the default ordering to use another vector
 #' @param along_with  use this variable to roll the function based on the  \code{[along_with - n, along_with]} rather than past \code{n} rows. NA are not accepted
+#' @param closed Logical of length 2 (recycled) Should interval be closed ? Default to c(TRUE, TRUE)
 #' @param ... options to pass to the function \code{FUN}
 #' @examples
 #' along_with  = c(1, 2, 4, 7)
@@ -14,19 +15,26 @@
 #' roll_lag(x, sum, n = 2, along_with = along_with)
 #' roll_lead(x, sum, n = 1, along_with = along_with)
 #' roll_lead(x, sum, n = 2, along_with = along_with)
-#' roll_lag(list(x,y), function(z){cor(z[[1]], z[[2]])},  n = 1, along_with = along_with)
+#' roll_lag(list(x,y), function(z){cor(z[[1]], z[[2]])},  n = 2, along_with = along_with)
 #' @export
 #' @aliases roll_lag roll_lead
 #' @rdname roll
 
 
 #' @rdname roll
-roll_lag <- function(x, FUN, n, along_with = NULL, order_by = NULL, closed.left = TRUE, ...){
-     if (!is.null(order_by)){
-       return(with_order(order_by, roll_lag, x, FUN = FUN, n = n, ...))
-    } else if (!is.null(along_with)){
-        l <- length(along_with)
-        ord <- order(along_with)
+roll_lag <- function(x, FUN, n, along_with = NULL, order_by = NULL, closed = c(TRUE, TRUE), ...){
+    if (length(closed)==1){
+        closed = rep(closed,2)
+    }
+    FUN <- match.fun(FUN)
+     if (!is.null(order_by) | !is.null(along_with)){
+        if (!is.null(order_by)){
+            sort <- order_by
+        } else{
+            sort <- along_with
+        }
+        l <- length(sort)
+        ord <- order(sort)
         undo <- match(seq_len(l), ord)
         if (is.matrix(x) | is.data.frame(x)){
             x <- x[ord,, drop = FALSE]
@@ -35,51 +43,86 @@ roll_lag <- function(x, FUN, n, along_with = NULL, order_by = NULL, closed.left 
         } else{
             x <- x[ord]
         }
-        along_with <- along_with[ord]
-        if (closed.left){
-            f_start <- .bincode(along_with-n, c(-Inf, along_with))
-        } else{
-            f_start <- findInterval(along_with-n, c(-Inf, along_with))
-        } 
-        f_end <- seq_len(l)
-        if (is.list(x)){
-            out <- mapply(function(start, end){
-                x <- lapply(x,function(z){z[seq.int(start,end)]})
-                FUN(x,...)
-                }, 
-            f_start, f_end)
-        } else if (is.matrix(x)|is.data.frame(x)){
-            out <- mapply(function(start, end){
-                FUN(x[seq.int(start,end),, drop = FALSE],...)
-                }, 
-            f_start, f_end)
-        }
-         else{
-            out <- mapply(function(start, end){
-                FUN(x[seq.int(start,end)],...)
-            }, 
-            f_start, f_end)
-        }
-        return(out[undo])
+        sort <- sort[ord]
     } else{
-        end <- seq_along(x)
-        start <- pmax(end-n,0)
-        return(mapply(function(start, end){
-            index <- start:end
-            FUN(x[index],...)
-        }, start, end))
+        if (!is.null(nrow(x))){
+            l <- nrow(x)
+        } else{
+            l <- length(x)
+        }
+    }
+    seq = seq_len(l)
+    if (!is.null(along_with)){
+        if (closed[[1]]){
+            f_start <- .bincode(sort-n, c(-Inf, sort))
+        } else{
+            f_start <- findInterval(sort-n, c(-Inf, sort))
+        } 
+        if (closed[[2]]){
+            vec <- lapply(seq, function(i) f_start[i]:i)
+        } else{
+            seq[seq <=  f_start] <- NA
+            vec <- lapply(seq, function(i) if (!is.na(i)) (f_start[i]:(i-1)))
+        }
+    } else {
+        if (closed[[1]]){
+            f_start <- pmax(seq_len(l) - n, 1)
+        } else{
+            f_start <- pmax(seq_len(l) - n + 1, 1)
+        } 
+        if (closed[[2]]){
+            vec <- lapply(seq, function(i) f_start[i]:i)
+        } else{
+            seq[seq <=  f_start] <- NA
+            vec <- lapply(seq, function(i) if (!is.na(i)) f_start[i]:(i-1))
+        }
+    }
+    if (is.list(x)){
+        out <- lapply(vec, function(v){
+            if (!is.null(v)){
+                x <- lapply(x,function(z){z[v]})
+                FUN(x,...)
+            } else NA
+            })
+    } else if (is.matrix(x)|is.data.frame(x)){
+        out <- lapply(vec, function(v){
+            if (!is.null(v)){
+                FUN(x[v,, drop = FALSE],...)
+            } else NA
+        })
+    }
+     else{
+        out <- lapply(vec, function(v){
+            if (!is.null(v)){
+                   FUN(x[v],...)
+               } else NA
+           })
+    }
+    out <- simplify2array(out, higher = TRUE)
+    if (!is.null(order_by) | !is.null(along_with)){
+        out[undo]
+    } else{
+        out
     }
 }
 
 
 
-#' @rdname roll
-roll_lead <- function(x, FUN, n, along_with = NULL, order_by = NULL, closed.left = TRUE, ...){
-     if (!is.null(order_by)){
-       return(with_order(order_by, roll_lag, x, FUN = FUN, n = n, ...))
-    } else if (!is.null(along_with)){
-        l <- length(along_with)
-        ord <- order(along_with)
+
+
+roll_lead <- function(x, FUN, n, along_with = NULL, order_by = NULL, closed = c(TRUE, TRUE), ...){
+    if (length(closed)==1){
+        closed = rep(closed,2)
+    }
+    FUN <- match.fun(FUN)
+     if (!is.null(order_by) | !is.null(along_with)){
+        if (!is.null(order_by)){
+            sort <- order_by
+        } else{
+            sort <- along_with
+        }
+        l <- length(sort)
+        ord <- order(sort)
         undo <- match(seq_len(l), ord)
         if (is.matrix(x) | is.data.frame(x)){
             x <- x[ord,, drop = FALSE]
@@ -88,89 +131,72 @@ roll_lead <- function(x, FUN, n, along_with = NULL, order_by = NULL, closed.left
         } else{
             x <- x[ord]
         }
-        along_with <- along_with[ord]
-        if (closed.right){
-                f_end <- findInterval(along_with, along_with - n)
-        } else{
-                 f_end <- .bincode(along_with, c(along_with-n, Inf))
-        } 
-        f_end <- seq_len(l)
-        if (is.list(x)){
-            out <- mapply(function(start, end){
-                x <- lapply(x,function(z){z[seq.int(start,end)]})
-                FUN(x,...)
-                }, 
-            f_start, f_end)
-        } else if (is.matrix(x) | is.data.frame(x)){
-            out <- mapply(function(start, end){
-                FUN(x[seq.int(start,end),, drop = FALSE],...)
-                }, 
-            f_start, f_end)
-        }
-         else{
-            out <- mapply(function(start, end){
-                FUN(x[seq.int(start,end)],...)
-            }, 
-            f_start, f_end)
-        }
-        return(out[undo])
+        sort <- sort[ord]
     } else{
-        end <- seq_along(x)
-        start <- pmax(end-n,0)
-        return(mapply(function(start, end){
-            index <- start:end
-            FUN(x[index],...)
-        }, start, end))
+        if (!is.null(nrow(x))){
+            l <- nrow(x)
+        } else{
+            l <- length(x)
+        }
+    }
+    seq = seq_len(l)
+    if (!is.null(along_with)){
+       if (closed[[2]]){
+            f_end <- findInterval(sort, sort - n)
+          } else{
+            f_end <- .bincode(sort, c(sort-n, Inf))
+          } 
+          if (closed[[1]]){
+              vec <- lapply(seq, function(i) i:f_end[i])
+          } else{
+              seq[seq >=  f_end] <- NA
+              vec <- lapply(seq, function(i) if (!is.na(i)) (i+1):f_end[i])
+          }
+    } else {
+         if (closed[[1]]){
+             f_end <- pmin(f + n, l)
+         } else{
+             f_end <- pmin(f + n - 1, l)-1
+         } 
+         if (closed[[2]]){
+            vec <- lapply(seq, function(i) i:f_end[i])
+         } else{
+            seq[seq > f_end] <- NA
+             vec <- lapply(seq, function(i) if (!is.na(i)) (i+1):f_end[i])
+         }
+    }
+    if (is.list(x)){
+        out <- lapply(vec, function(v){
+            if (!is.null(v)){
+                x <- lapply(x,function(z){z[v]})
+                FUN(x,...)
+            } else NA
+            })
+    } else if (is.matrix(x)|is.data.frame(x)){
+        out <- lapply(vec, function(v){
+            if (!is.null(v)){
+                FUN(x[v,, drop = FALSE],...)
+            } else NA
+        })
+    }
+     else{
+        out <- lapply(vec, function(v){
+            if (!is.null(v)){
+                   FUN(x[v],...)
+               } else NA
+           })
+    }
+    out <- simplify2array(out, higher = TRUE)
+    if (!is.null(order_by) | !is.null(along_with)){
+        out[undo]
+    } else{
+        out
     }
 }
 
 
 
-#roll_lag <- function(x, n, along_with = NULL, order_by = NULL, closed.left = TRUE){
-#    roll_lag_(lazy(x),  n, along_with = along_with, order_by =  order_by, closed.left = closed.left)
-#}
-#
-#roll_lag_ <- function(vars, n, along_with = NULL, order_by = NULL, closed.left = TRUE){
-#     if (!is.null(order_by)){
-#       return(with_order(order_by, roll_lag_, vars, n = n, closed.left = TRUE))
-#    } else{
-#        names <- all.vars(vars$expr)
-#        print(vars)
-#        env <- common_env(vars)
-#        print(env)
-#        print(ls(env))
-#        names_right <- names[length(get(names, env))==length(along_with)]
-#        m <- sapply(names_right, function(x){get(x, env)})
-#        values <- lapply(seq_along(names_right), function(x){substitute(m[, x], list(x = x))})
-#        expr <- interp(vars$expr, .values = setNames(values, names_right))
-#        if (!is.null(along_with)){
-#        l <- length(along_with)
-#        ord <- order(along_with)
-#        undo <- match(seq_len(l), ord)
-#        x <- x[ord]
-#        along_with <- along_with[ord]
-#        if (closed.left){
-#            f_start <- .bincode(along_with-n, c(-Inf, along_with))
-#        } else{
-#            f_start <- findInterval(along_with-n, c(-Inf, along_with))
-#        } 
-#        f_end <- seq_len(l)
-#        out <- mapply(
-#            function(start, end){
-#                env$m <- m[seq.int(start,end),, drop = FALSE]
-#                eval(expr, env)
-#            },
-#            f_start, f_end)
-#        return(out[undo])
-#    } else{
-#        end <- seq_along(x)
-#        start <- pmax(end-n,0)
-#        return(mapply(function(start, end){
-#            index <- start:end
-#            FUN(x[index],...)
-#        }, start, end))
-#    }
-#    }
-#}
+
+
 
 
