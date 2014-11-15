e717b73286ad3e266d03bfbbe2f8f42bc521a3f9
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2014-11-15 14:53:24 -0500

    sample_mode uses forderv too

--- R/sample_mode.R
+++ R/sample_mode.R
@@ -2,7 +2,7 @@
 #' 
 #' @param x A vector of values
 #' @param na.rm Should NA removed?
-#' @return Returns one mode of the vector (in case of ties, the first value is chosen)
+#' @return Returns one mode of the vector (in case of ties, the first value by order of appearance is chosen)
 #' @examples                        
 #' sample_mode(c(1, 2, 2))
 #' sample_mode(c(1, 2))
@@ -11,6 +11,10 @@
 #' @export
 sample_mode <- function(x, na.rm = TRUE) {
   if (na.rm) x <- x[!is.na(x)]
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
+  order <- data.table:::forderv(x, retGrp = TRUE)
+  size <- diff(c(attr(order,"starts"), length(x) + 1))
+  if (!length(order)){
+  	order <- seq.int(1, length(x))
+  }
+  x[order[which.max(size)]]
 }
