59ff3d8a3bb70450b3f00868e402f6a2d7e4e530
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2014-12-04 15:13:06 -0500

    better handling when no match

--- R/setkeep.R
+++ R/setkeep.R
@@ -190,10 +190,15 @@ discard_if_ <- function(x, .dots, by = NULL){
 	dots <-  lazyeval::all_dots(.dots)
 	expr <- lapply(dots, `[[`, "expr")
 	call <- substitute(dt[, .I[expr], by = byvars], list(expr=or_expr(expr)))
-	env <- dt_env(x, lazyeval::common_env(dots), byvars = byvars)
-	ans <- eval(call, env)
-	indices <- ans[[length(ans)]]
-	x[-indices[!is.na(indices)]]
+	if (!length(call)){
+		out <- x
+	} else{
+		env <- dt_env(x, lazyeval::common_env(dots), byvars = byvars)
+		ans <- eval(call, env)
+		indices <- ans[[length(ans)]]
+		out <- x[-indices[!is.na(indices)]]
+	}
+	out
 }
 
 
