a4010dfe0ad1bc64228aa20ebbb9d02369386dab
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-06-06 17:21:57 -0400

    group missing

--- R/group.R
+++ R/group.R
@@ -5,9 +5,18 @@
 #' @examples 
 #' library(dplyr)                   
 #' mutate(iris, g = group(Species))
-#' mutate(iris, g = group(Species, floor(Sepal.Width)))
+# returns NA for groups where some variable is missing
+#' mutate(iris, g = group(Species, floor(Sepal.Width)), na.rm = TRUE)
 #' @export
-group <- function(...){
-  df <- data.frame(..., stringsAsFactors = FALSE)
-  df %>% group_by_(.dots = names(df)) %>% group_indices
+group <- function(..., na.rm = FALSE){
+  df <- data.frame(list(...))
+  if (na.rm){
+  	out <- rep(NA, nrow(df))
+  	complete <- complete.cases(df)
+  	indices <- df %>% filter(complete) %>% group_indices_(.dots = names(df))
+  	out[complete] <- indices
+  } else{
+  	out <- group_indices_(df, .dots = names(df))
+  }
+  out
 }
