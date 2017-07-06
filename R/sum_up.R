8476831550f86325a136c355c204ae7a92026cc7
gvelasq2 <gvelasq2@users.noreply.github.com>
2017-07-06 15:37:30 -0400

    Travis documentation fix

--- R/sum_up.R
+++ R/sum_up.R
@@ -1,7 +1,7 @@
 #' Gives summary statistics (corresponds to Stata command \code{summarize})
 #' 
 #' @param df A data frame
-#' @param ... Variables to include. Defaults to all non-grouping variables. See the \code{\link[dplyr]{select()}} documentation.
+#' @param ... Variables to include. Defaults to all non-grouping variables. See the \code{\link[=dplyr]{select()}} documentation.
 #' @param d Should detailed summary statistics be printed?
 #' @param wt Frequency weights. Default to \code{NULL}.
 #' @examples
