89be39d3cf959c8ee14288e6404165d86820c49e
Erik Loualiche <erikl@mit.edu>
2017-07-06 09:37:44 -0400

    - move wt -> w

--- R/pctile.R
+++ R/pctile.R
@@ -5,25 +5,25 @@
 #' @param w A weight vector
 #' @param na.rm Should missing values be returned?
 #' @export
-pctile <- function(x, probs = c(0.25, 0.5, 0.75), w = NULL, na.rm = FALSE){
+pctile <- function(x, probs = c(0.25, 0.5, 0.75), wt = NULL, na.rm = FALSE){
   if (is.null(w)){
     quantile(x = x, type = 2, probs = probs, na.rm = na.rm)
   } else{
-      if (anyNA(x) | anyNA(w)) {
+      if (anyNA(x) | anyNA(wt)) {
         if (na.rm) {
-          na <- is.na(x) | is.na(w)
+          na <- is.na(x) | is.na(wt)
           x <- x[!na]
-          w <- w[!na]
+          wt <- wt[!na]
         }
         else{
           stop("Missing values not allowed when na.rm is FALSE", call. = FALSE)
-        } 
+        }
       }
       # Ensure x and w in ascending order of x
       order <- order(x)
-      cumsum <- cumsum(w[order])
+      cumsum <- cumsum(wt[order])
       n <- cumsum[length(cumsum)]
-      # follow definition of quantile 2 
+      # follow definition of quantile 2
       index <- n * probs
       j <- floor(index)
       low <- x[order[pmin(length(x),   .bincode(j, c(-Inf, cumsum)))]]
